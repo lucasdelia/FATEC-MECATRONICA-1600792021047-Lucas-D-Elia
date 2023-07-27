c86e50fde69452c6745279d90e24ca846e706075
Lucas D'Elia da Silva <71405756+lucasdelia@users.noreply.github.com>
2023-07-27 09:38:08 -0300

    Create EscalaCinzaImagem.py

---
+++ Robótica_Ind./EscalaCinzaImagem.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+img = cv2.imread("eobrad.jpg",0)
+ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
+ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
+ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
+ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
+
+cv2.imshow("eobrad.jpg", thresh5)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
