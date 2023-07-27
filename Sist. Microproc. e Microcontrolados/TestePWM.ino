eca973baec7fcaa5112f2e782af17ee23c84fed5
Lucas D'Elia da Silva <71405756+lucasdelia@users.noreply.github.com>
2023-07-27 10:08:27 -0300

    Create TestePWM.ino

---
+++ Sist. Microproc. e Microcontrolados/TestePWM.ino
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+
+//Controle do PWM
+
+//Define o pino que será usado como PWM
+
+const int PWM0 = 23;
+const int PWM1 = 22;
+
+//Ajuste os parâmetros do PWM
+const int freqPWM = 2000;
+const int canalPWM0 = 0;
+const int canalPWM1 = 1;
+const int resolucao = 10;
+
+#define IN1 5
+#define IN2 0
+#define IN3 13
+#define IN4 14
+
+void setup() 
+{
+  ledcSetup(canalPWM0, freqPWM, resolucao);
+  ledcSetup(canalPWM1, freqPWM, resolucao);  
+  
+  ledcAttachPin(5, canalPWM0);
+  ledcAttachPin(14, canalPWM0);
+  
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() { 
+ ledcWrite(canalPWM0, 1023);
+ delay(1000);
+ ledcWrite(canalPWM0, 0);
+ delay(200);
+ ledcWrite(canalPWM0, 511);
+ delay(1000);
+ ledcWrite(canalPWM0, 0);
+ delay(200);
+}
